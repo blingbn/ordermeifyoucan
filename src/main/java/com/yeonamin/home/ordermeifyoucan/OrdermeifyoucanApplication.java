c246a2b9a5b7342792bcb262839e9dd6ecd67ec4
min <www5981@naver.com>
2022-02-23 12:31:54 +0900

    스프링부트 프로젝트 생성

---
+++ src/main/java/com/yeonamin/home/ordermeifyoucan/OrdermeifyoucanApplication.java
@@ -0,0 +1,13 @@
+package com.yeonamin.home.ordermeifyoucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermeifyoucanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermeifyoucanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeonamin/home/ordermeifyoucan/OrdermeifyoucanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonamin.home.ordermeifyoucan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermeifyoucanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
